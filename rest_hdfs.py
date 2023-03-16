aeab2a7284c8f1ae46ad6d74e2867e430d127508
toiletbril <s9und@outlook.com>
2023-03-16 08:49:25 +0300

    Try one

--- rest_hdfs.py
+++ rest_hdfs.py
@@ -17,7 +17,10 @@ class InsecureClient:
     def _get(self, path: str, op: str) -> dict:
         adr = self._adress + self._api
         content = b""
-        with rq.get(f"{adr}{path}?user.name={self._user}&op={op}") as res:
+
+        url = adr + path + f"user.name={self._user}&op={op}"
+
+        with rq.get(url) as res:
             content = res.content
         return loads(content)
 
@@ -25,83 +28,97 @@ class InsecureClient:
 
         adr = self._adress + self._api
         content = b""
-        link = ""
 
-        with rq.get(f"{adr}{path}?user.name={self._user}&op={op}", allow_redirects=False) as res:
-            link = res.headers.get("Location")
-
-        with rq.get(link, allow_redirects=False) as res:
+        with rq.get(f"{adr}{path}?user.name={self._user}&op={op}") as res:
             content = res.content
+
         return content
-    
+
     def _delete(self, path: str, op: str, recursive: bool) -> dict:
+
         adr = self._adress + self._api
         content = b""
-        with rq.delete(f"{adr}{path}?user.name={self._user}&op={op}" 
-                       + ("&recursive=true" if recursive else "")) as res:
+
+        recursive = "&recursive=true" if recursive else ""
+        url = adr + path + f"user.name={self._user}&op={op}"
+
+        with rq.delete(url + recursive) as res:
             content = res.content
+
         return loads(content)
-    
+
     def _post(self, path: str, op: str, data: bytes):
+
         adr = self._adress + self._api
         content = b""
-        with rq.post(f"{adr}{path}?user.name={self._user}&op={op}") as res:
+
+        url = adr + path + f"user.name={self._user}&op={op}"
+
+        with rq.post(url) as res:
             content = res.content
+
         return loads(content)
 
     def _post_append(self, path: str, op: str, data: bytes):
 
         adr = self._adress + self._api
         content = b""
-        link = ""
 
-        with rq.post(f"{adr}{path}?user.name={self._user}&op={op}",
-                     allow_redirects=False) as res:
-            link = res.headers.get("Location")
+        headers = {"Content-Type": "application/octet-stream"}
 
-        with rq.post(link, data=data, allow_redirects=False) as res:
-            content = res.content
+        url = adr + path + f"user.name={self._user}&op={op}"
+
+        with rq.post(url, data, headers=headers) as res:
+            print(res.content)
+
         return content
 
     def _put(self, path: str, op: str, args: str = None) -> dict:
         adr = self._adress + self._api
         content = b""
-        with rq.put(f"{adr}{path}?user.name={self._user}&op={op}" + (args or ""), allow_redirects=False) as res:
+
+        args = args or ""
+        url = adr + path + "user.name={self._user}&op={op}" + args
+
+        with rq.put(url, allow_redirects=False) as res:
             content = res.content
+
         return loads(content)
 
-    def _put_touch(self, path: str, op: str, args: str = None, data: bytes = None) -> dict:
+    def _put_create(self, path: str, op: str, args: str = None, data: bytes = None) -> dict:
 
         adr = self._adress + self._api
         content = b""
-        link = ""
 
-        with rq.put(f"{adr}{path}?user.name={self._user}&op={op}" + (args if args else ""), allow_redirects=False) as res:
-            link = res.headers.get("Location")
+        args = args or ""
+        headers = {"Content-Type": "application/octet-stream"} + data
 
-        with rq.put(link, data if data else None, allow_redirects=False) as res:
-            content = res.content
+        url = adr + path + f"user.name={self._user}&op={op}" + args
+
+        with rq.put(url, data, headers=headers) as res:
+            print(res.content)
+
         return content
 
     def ls(self, path: str) -> dict:
         return self._get(path, "LISTSTATUS")
-    
+
     def status(self, path: str) -> dict:
         return self._get(path, "GETFILESTATUS")
-    
+
     def delete(self, path: str, recursive: bool = False):
         return self._delete(path, "DELETE", recursive)
-    
+
     def mkdir(self, path: str, perm: int = 600):
         return self._put(path, "MKDIRS", f"&permission={perm}")
-    
-    def touch(self, path):
-        return self._put_touch(path, "CREATE")
-    
+
+    def touch(self, path: str):
+        return self._put_create(path, "CREATE")
+
     # This works the same way as GET & PUT
     def append(self, path: str, data: bytes):
         return self._post_append(path, "APPEND", data)
-    
+
     # NOTE: GET & PUT do not work if you didn't configure endpoint for TEMPORARY_REDIRECT
     def open(self, path):
         return self._get_open(path, "OPEN")
@@ -113,4 +130,3 @@ class InsecureClient:
 
     def upload(self, path, filename: str, data: bytes):
         return self._put_touch(path + "/" + filename, "CREATE", data=data)
-
